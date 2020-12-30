eccb609f54b239bad5f37e754a4986ccb442bc4c
rafael-pi <76755695+rafael-pi@users.noreply.github.com>
2020-12-30 19:36:18 -0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below will cache a inverse of a matrix
+## The first one creates a special matrix that can cache its inverse
+## the second one checks if the inverse was calculated and save some
+## data computing
 
-## Write a short comment describing this function
+# for inverse calculation, we need the matlib package
+install.packages("matlib")
+library(matlib)
+
+## Lets create a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    datasetinverse <- NULL                   
+    set <- function(y) {
+            x<<-y
+            datasetinverse <<- NULL
+    }
+    get <- function() {x}
+    setinverse <- function(solve) {datasetinverse <<- solve(x)}
+    getinverse <- function() {datasetinverse}
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+    
 }
 
 
-## Write a short comment describing this function
+## This function checks if the inverse was already calculated
+## in this way we can save some time in our data processing
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    datasetinverse <- x$getinverse()
+    if(!is.null(datasetinverse)){
+        message("getting cached data")
+        return(datasetinverse)
+    }
+  
+    mat <- x$get()
+    inv <- solve(mat, ...)
+    x$setinverse(datasetinverse)
+    inv
 }
+        ## Return a matrix that is the inverse of 'x'
